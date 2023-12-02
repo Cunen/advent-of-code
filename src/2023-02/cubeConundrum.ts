53f88f223d7bdf9bb28335f019133cdd09fa3f79
Cunen <xmetallesx@gmail.com>
2023-12-02 11:31:44 +0200

    Optimize 2023 day 02

--- src/2023-02/cubeConundrum.ts
+++ src/2023-02/cubeConundrum.ts
@@ -1,57 +1,27 @@
 import { prodInput } from "./input";
 
-type Color = "red" | "green" | "blue";
-
-const cubeLimits: Record<Color, number> = {
-  red: 12,
-  green: 13,
-  blue: 14,
+const reducer = (game: string, color: string) => {
+  const regex = new RegExp(`\\b(\\d+)\\s+${color}\\b`, "g");
+  return (
+    game.match(regex)?.reduce((a, b) => {
+      const num = Number(b.split(" ")[0]);
+      return num > a ? num : a;
+    }, 0) ?? 0
+  );
 };
 
 export const cubeConundrum = () => {
-  const games = prodInput.split("\n");
-  const summary = { idSum: 0, squareSum: 0 };
-  const gameSum = games.reduce((sum, game) => {
-    // Game ID
-    const id = Number(game.split(":")[0].replace(/[A-Za-z]+/, ""));
-    // Parse rounds
-    const rounds = game.replace(/Game [0-9]+: /, "").split(";");
-
-    let bMax = 0;
-    let gMax = 0;
-    let rMax = 0;
-
-    let fault = false;
-    rounds.forEach((round) => {
-      const trimmed = round.trim();
-      const cubes = trimmed.split(", ").map((c) => c.split(" "));
-
-      let faultyRoll = false;
-      cubes.forEach((roll) => {
-        const count = Number(roll[0]);
-        const color = roll[1] as Color;
-
-        if (color === "green" && count > gMax) gMax = count;
-        if (color === "red" && count > rMax) rMax = count;
-        if (color === "blue" && count > bMax) bMax = count;
-
-        if (count > cubeLimits[color]) faultyRoll = true;
-      });
-
-      if (faultyRoll) fault = true;
-    });
-
-    return {
-      idSum: (!fault ? id : 0) + sum.idSum,
-      squareSum: sum.squareSum + (bMax * gMax * rMax),
-    };
-  }, summary);
-
-  console.log(gameSum);
-
-  // Part 1
-  console.log("Part 1:", gameSum.idSum);
-
-  // Part 2
-  console.log("Part 2:", gameSum.squareSum);
+  const gameSum = prodInput.split("\n").reduce(
+    (sum, game) => {
+      const gameId = Number(game.split(":")[0].replace(/[A-Za-z]+/, ""));
+      // prettier-ignore
+      const [r, g, b] = [reducer(game, "red"), reducer(game, "green"), reducer(game, "blue")];
+      const idSum = (r > 12 || g > 13 || b > 14 ? 0 : gameId) + sum.idSum;
+      const squareSum = sum.squareSum + r * g * b;
+      return { idSum, squareSum };
+    },
+    { idSum: 0, squareSum: 0 }
+  );
+  console.log("Part 1:", gameSum.idSum); // Part 1
+  console.log("Part 2:", gameSum.squareSum); // Part 2
 };
