0745493b82202b82d8717854cd33dfd7674ec8c5
kkuparinen <kkuparinen@topcon.com>
2023-12-21 12:24:53 +0200

    implement 2023 day 17 Part 1

--- src/2023-17/clumsyCrucible.ts
+++ src/2023-17/clumsyCrucible.ts
@@ -5,7 +5,7 @@ const findShortestPath = (matrix: number[][]) => {
   const visited: Record<string, number> = {};
   let lowestSoFar = Infinity;
   let counter = 0;
-  const go = (x: number, y: number, sum: number, block: string): number[] => {
+  const go = (x: number, y: number, sum: number, block: string, str = ''): number[] => {
     counter++;
     if (counter % 100000000 === 0) {
       console.log(
@@ -20,18 +20,21 @@ const findShortestPath = (matrix: number[][]) => {
     const last3 = block.substring(0, 3);
     const backwards =
       last3 === "rlr" || last3 === "lrl" || last3 === "udu" || last3 === "dud";
+    const l1 = block === 'ruld' || block === 'rdlu' || block === 'ldru' || block === 'lurd'; 
+    const l2 = block === 'urdl' || block === 'uldr' || block === 'dlur' || block === 'drul'; 
+    const loop = l1 || l2;
     const tooFar =
       block === "rrrr" ||
       block === "llll" ||
       block === "uuuu" ||
       block === "dddd";
-    const tooBig = sum > lowestSoFar || sum > 970;
+    const tooBig = sum > lowestSoFar || sum > 963;
 
     const expectedFactor = 4.25;
     const xyPrune = x + y > 10 && sum > (x + y) * expectedFactor;
 
     // Run Stopped
-    if (outOfBounds || tooFar || tooBig || backwards || xyPrune) return [];
+    if (outOfBounds || tooFar || tooBig || backwards || loop || xyPrune) return [];
     // Run Complete
     const len = matrix.length - 1;
     if (x === len && y === len) {
@@ -43,13 +46,13 @@ const findShortestPath = (matrix: number[][]) => {
     const newSum = sum + matrix[y][x];
     const dirKey = block + key;
     const canContinue = newSum < (visited[dirKey] || Infinity);
-    if (!canContinue) return [];
+    if (!canContinue || str.includes(key)) return [];
     visited[dirKey] = newSum;
 
-    const right = go(x + 1, y, newSum, "r" + block.substring(0, 3));
-    const down = go(x, y + 1, newSum, "d" + block.substring(0, 3));
-    const left = go(x - 1, y, newSum, "l" + block.substring(0, 3));
-    const up = go(x, y - 1, newSum, "u" + block.substring(0, 3));
+    const right = go(x + 1, y, newSum, "r" + block.substring(0, 3), str + '|' + key);
+    const down = go(x, y + 1, newSum, "d" + block.substring(0, 3), str + '|' + key);
+    const left = go(x - 1, y, newSum, "l" + block.substring(0, 3), str + '|' + key);
+    const up = go(x, y - 1, newSum, "u" + block.substring(0, 3), str + '|' + key);
 
     return [...right, ...left, ...down, ...up];
   };
@@ -64,9 +67,6 @@ export const crucible = async () => {
     Array.from(row).map(Number)
   );
 
-  // (1016) too high
-  // (1002) too high
-  // (960) is wrong
-  // (963) is correct
+  // (963) is correct (245281.4 ms)
   console.log("Part 1:", findShortestPath(array));
 };
